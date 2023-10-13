dfcb664f5715aa2da4cf8083d54cee768d4519e2
yun-setenforce <extendsabstractclass@gmail.com>
2023-10-13 17:04:11 +0900

    DTO, Entity 매핑 완료 / member-로그인, 회원가입 구현

--- src/main/java/co/kr/lotteon/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package co.kr.lotteon.repository;
-
-import co.kr.lotteon.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-
-    int countByUid(String uid);
-}
