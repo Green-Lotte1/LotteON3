63406f2c45400e204cf41da91fd92d9bd6f7a73d
yun-setenforce <extendsabstractclass@gmail.com>
2023-10-13 15:19:54 +0900

    first commit

---
+++ src/main/java/co/kr/lotteon/security/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package co.kr.lotteon.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+				// 사이트 위변조 방지 비활성
+				.csrf(CsrfConfigurer::disable) // 메서드 참조 연산자로 람다식을 간결하게 표현
+				// 폼 로그인 설정
+				.formLogin(config -> config.loginPage("/member/login")
+						.defaultSuccessUrl("/", true)
+						.failureUrl("/member/login?success=100")
+						.usernameParameter("uid")
+						.passwordParameter("pass"))
+
+				// 로그아웃 설정
+				.logout(config -> config
+						.logoutUrl("/member/logout")
+						.invalidateHttpSession(true)
+						.clearAuthentication(true)
+						.logoutSuccessUrl("/member/login?success=200"))
+
+				// 인가 권한 설정
+				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+						.requestMatchers("/").authenticated()
+						.requestMatchers("/css/**", "/js/**", "/images/**", "/thumb/**", "/upload/**").permitAll());
+
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+
+}
