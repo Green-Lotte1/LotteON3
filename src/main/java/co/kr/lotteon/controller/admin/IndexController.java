97b91461f65898e236aac511e787632480e2c377
z82714567 <z82714567@gmail.com>
2023-10-15 19:50:16 +0900

    admin

---
+++ src/main/java/co/kr/lotteon/controller/admin/IndexController.java
@@ -0,0 +1,13 @@
+package co.kr.lotteon.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value ="/admin/index")
+    public String AdminIndex(){
+        return "/admin/index";
+    }
+}
