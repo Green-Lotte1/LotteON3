9598966b51848637e168a6ec1b417839dd569e0c
iisk145 <iisk145@gmail.com>
2023-10-15 21:04:54 +0900

    프로덕트 레이아웃작업 및 인덱스 css, img 적용 경로 변경

---
+++ src/main/java/co/kr/lotteon/controller/product/CartController.java
@@ -0,0 +1,17 @@
+package co.kr.lotteon.controller.product;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Log4j2
+@RequiredArgsConstructor
+@Controller
+public class CartController {
+
+    @GetMapping("/product/cart")
+    public String cart() {
+        return "/product/cart";
+    }
+}
