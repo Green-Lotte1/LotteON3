a75640f1070800399ceecfc71068db7c819655fc
iisk145 <iisk145@gmail.com>
2023-10-15 21:04:54 +0900

    프로덕트 레이아웃작업 및 인덱스 css, img 적용 경로 변경

---
+++ src/main/java/co/kr/lotteon/repository/LtProductRepository.java
@@ -0,0 +1,11 @@
+package co.kr.lotteon.repository;
+
+
+import co.kr.lotteon.entity.LtProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LtProductRepository extends JpaRepository<LtProductEntity, Integer> {
+
+    
+
+}
