25bb1525412ba5356720d4fccb35535cd761c1ce
iisk145 <iisk145@gmail.com>
2023-10-18 14:31:21 +0900

    1018_오전작업_prod_list

--- src/main/java/co/kr/lotteon/controller/LtProductController.java
+++ src/main/java/co/kr/lotteon/controller/LtProductController.java
@@ -1,10 +1,18 @@
 package co.kr.lotteon.controller;
 
+import co.kr.lotteon.dto.LtProductDTO;
+import co.kr.lotteon.service.LtProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Log4j2
 @Controller
@@ -12,9 +20,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/product")
 public class LtProductController {
 
-    @GetMapping("/view")
-    public String view() {
+    private final LtProductService ltProductService;
+
+    @GetMapping("/view/{prodNo}")
+    public String view(@RequestParam(value = "prodNo") Model model, int prodNo) {
+
+        LtProductDTO dto = ltProductService.getProdDto(prodNo);
+
+        model.addAttribute("prodView", dto);
+
         return "/product/view";
+
     }
 
     @GetMapping("/complete")
@@ -22,8 +38,13 @@ public class LtProductController {
         return "/product/complete";
     }
 
+//todo    프로덕트 리스트 페이지 출력 테스트 완료 - 1018_11:23
     @GetMapping("/list")
-    public String list() {
+    public String list(Model model) {
+
+        List<LtProductDTO> productlist = ltProductService.selectProducts();
+        model.addAttribute("productlist",productlist);
+
         return "/product/list";
     }
 
